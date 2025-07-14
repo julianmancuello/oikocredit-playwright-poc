62d963a567665110b630684b0d6415655dfb7ec0
julianmancuello <162643807+julianmancuello@users.noreply.github.com>
2025-07-15 00:12:24 +0200

    assertion added

--- tests/firstTests.spec.ts
+++ tests/firstTests.spec.ts
@@ -1,8 +1,9 @@
-import { test } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import { PageManager } from "../src/pages/pageManager"
 
 test('first test', async ({page}) => {
   await page.goto("https://oikocredit--ltp.sandbox.my.salesforce.com/")
   const pm = new PageManager(page)
   await pm.onLoginPage().loginUser("jmancuello@oikocredit.org.ltp", "Nina23!!")
+  await expect(page).toHaveURL("https://oikocredit--ltp.sandbox.lightning.force.com/lightning/page/home")
 })
