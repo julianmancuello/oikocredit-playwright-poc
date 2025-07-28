ad78baa60e09addae2bd1cfd2940ba3bff2ca5b3
julianmancuello <162643807+julianmancuello@users.noreply.github.com>
2025-07-28 09:30:55 +0200

    url config added

--- src/pages/navigationManager.ts
+++ src/pages/navigationManager.ts
@@ -2,14 +2,30 @@ import { ContextStore as cs } from "../utils/contextStore"
 import { Environment, Application } from "../utils/platformUtils"
 import { page } from "../../features/step-definitions/hooks"
 
+const urls: Record<Application, Record<Environment, string>> = {
+  [Application.SALESFORCE]: {
+    [Environment.LTP]: 'https://oikocredit--ltp.sandbox.my.salesforce.com/',
+    [Environment.ACC]: 'https://acc.salesforce.com',
+  },
+  [Application.TITAN]: {
+    [Environment.LTP]: 'https://ltp.titan.io',
+    [Environment.ACC]: 'https://acc.titan.io',
+  }
+}
+
 export class NavigationManager {
 
   static async goToApp(app: Application, env: Environment) {
-    const key = `${app}-${env}-url`
-    const url = cs.get<string>(key)
+    const appUrls = urls[app]
 
-    if(!url) {
-      throw new Error(`URL for key '${key}' not found in ContextStore`)
+    if (!appUrls) {
+      throw new Error(`Application '${app}' not found in URL config`)
+    }
+
+    const url = appUrls[env];
+
+    if (!url) {
+      throw new Error(`URL for application '${app}' in environment '${env}' not found`)
     }
 
     await page.goto(url)
